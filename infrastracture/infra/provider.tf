ce4d1ec6ce46211de2aa5e420ace2d1911e4300c
jamiekariuki <jamiekariuki18@gmail.com>
2026-06-19 11:29:11 +0300

    feat: add eks provider and auth

--- infrastracture/infra/provider.tf
+++ infrastracture/infra/provider.tf
@@ -18,3 +18,38 @@ terraform {
     }
   }
 }
+
+
+
+# Get cluster info
+data "aws_eks_cluster" "this" {
+  name = module.eks.cluster_name
+
+  depends_on = [module.eks]
+}
+
+# Get authentication token
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+
+  depends_on = [module.eks]
+}
+
+# Kubernetes provider
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+
+}
+
+# Helm provider
+provider "helm" {
+  kubernetes ={
+    host                   = data.aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+}
+ 
+
